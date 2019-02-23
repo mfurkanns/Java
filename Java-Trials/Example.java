9fe89642ac65545ce6a86dc71dda185e9243cfb5
Furkan <sahinmehmet980@gmail.com>
2019-02-23 23:53:27 +0300

    Add files via upload

---
+++ Java-Trials/Example.java
@@ -0,0 +1,36 @@
+package dene;
+
+import java.util.*;
+
+public class Example {
+	
+	public static void main(String[] args) {
+		
+		int num,ran;
+		int[] arr;
+		
+		Scanner scan = new Scanner(System.in);
+		Random rand = new Random();
+		
+		System.out.print("enter the number of elements = ");
+		num = scan.nextInt();
+		arr = new int[num];
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.print((i+1)+".element =");
+			arr[i]=scan.nextInt();
+		}
+		
+		System.out.println(arr[rand.nextInt(num)]);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
