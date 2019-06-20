d2688a693b14dca85168eb314df278885f58a8df
Furkan <sahinmehmet980@gmail.com>
2019-06-20 18:06:56 +0300

    Update MultiDimensionalArrayList.java

--- Java_Fundamentals/MultiDimensionalArrayList.java
+++ Java_Fundamentals/MultiDimensionalArrayList.java
@@ -6,21 +6,14 @@ import java.util.Scanner;
 
 public class MultiDimensionalArrayList {
 	
-	
 	static {
 		System.out.println("Multidimensional ArrayList\n---------------------------");
 	}
 	
-	
 	public static void main(String[] args) {
-		
-		
-		
+	
 		ArrayList<ArrayList<Integer>> multiArrList = new ArrayList<ArrayList<Integer>>();
-		
-		
-		
-		
+	
 		// one space allowed for the 0th row
 		multiArrList.add(new ArrayList<Integer>());
 		multiArrList.add(new ArrayList<Integer>());
@@ -36,14 +29,10 @@ public class MultiDimensionalArrayList {
 		multiArrList.get(1).add(2,13);
 		multiArrList.get(3).add(0,2);
 		multiArrList.get(3).add(1,2);
-		
-		
+
 		System.out.println(multiArrList);
 		
-		multiArrList.get(0);
-		
-		
-		
+				
 		ArrayList<ArrayList<String>> multi = new ArrayList<ArrayList<String>>();
 		
 		
@@ -60,21 +49,6 @@ public class MultiDimensionalArrayList {
 		multi.get(4).add(1,"Banana");
 		multi.get(2).add(0,"Java");
 		
-		System.out.println(multi);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.println(multi);		
 	}
 }
